c06ee6af3c48c839ee1703dc9730071e2889c722
Theodore Chan <tchan@trinity.edu>
2025-10-28 14:11:04 -0500

    commit

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -20,7 +20,7 @@ const socialLinks = [
 
 export const SocialLinks = () => {
   return (
-    <div className="flex gap-6 mt-6">
+    <div className="flex gap-6 mt-24">
       {socialLinks.map((link) => {
         const Icon = link.icon;
         return (
